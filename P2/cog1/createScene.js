2a963d49f3e3e7ad7fa37b64f196052a983469a2
Steffi <94295160+SteffiHe@users.noreply.github.com>
2022-09-22 17:11:15 +0200

    myModel

--- P2/cog1/createScene.js
+++ P2/cog1/createScene.js
@@ -17,11 +17,12 @@ function(exports, scenegraph, animation) {
 	 */
 	function init() {
 
-		var cubeNode = scenegraph.createNodeWithModel("cube", "myModel", {scale:300});
+		var cubeNode = scenegraph.createNodeWithModel("cube", "cube", {scale:300});
 		cubeNode.rotateTo([1.6, -3.87, 0]);
 
 		// BEGIN exercise myModel
-		
+		var cubeNode = scenegraph.createNodeWithModel("cube", "myModel", {scale:300});
+		cubeNode.rotateTo([1.6, -3.87, 0]);
 		// END exercise myModel
 		
 		return;
